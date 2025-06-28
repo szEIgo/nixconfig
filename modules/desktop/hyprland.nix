6a02b62a2adec7730cfad4fa404909e68dc7c877
jsz-nine <jsz@nine.dk>
2025-06-28 21:22:15 +0200

    hyperland

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -12,4 +12,17 @@
   environment.systemPackages = [
     pkgs.kitty
   ];
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings."org/gnome/desktop/interface" = {
+        gtk-theme = "Adwaita";
+        icon-theme = "Flat-Remix-Red-Dark";
+        font-name = "Noto Sans Medium 11";
+        document-font-name = "Noto Sans Medium 11";
+        monospace-font-name = "Noto Sans Mono Medium 11";
+      };
+    }
+  ];
+
 }
