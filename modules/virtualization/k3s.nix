0fc2414e051d966a908e06351005126ab5d2d9f7
joni <jonathan@szigethy.dk>
2025-12-07 18:51:36 +0100

    working, but needs alot of configuration

--- modules/virtualization/k3s.nix
+++ modules/virtualization/k3s.nix
@@ -11,12 +11,4 @@
   networking.firewall.allowedTCPPorts = [ 6443 ];
   networking.firewall.allowedUDPPorts = [ 8472 ];
 
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = true;
-    "net.netfilter.nf_conntrack_max" = 131072;
-  };
-
-  environment.systemPackages = with pkgs; [ kubernetes.kubectl ];
-  environment.etc."kubeconfig".source = config.services.k3s.kubeconfig;
-  environment.sessionVariables = { KUBECONFIG = "/etc/kubeconfig"; };
 }
