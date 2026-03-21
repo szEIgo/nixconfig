5f584fffc81775803605829e3a6d7aca20a33234
joni <jonathan@szigethy.dk>
2026-03-21 22:47:26 +0100

    updated hardware for nuc

--- hosts/nuc/hardware.nix
+++ hosts/nuc/hardware.nix
@@ -10,7 +10,7 @@
   ];
 
   boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+    [ "xhci_pci" "ehci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
