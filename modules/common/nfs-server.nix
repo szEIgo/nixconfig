ceb4934bb2cce92cfaf80e02cc692dd9e7529339
joni <jonathan@szigethy.dk>
2026-03-22 21:52:21 +0100

    adding 10.100.0.0/24 to nfs

--- modules/common/nfs-server.nix
+++ modules/common/nfs-server.nix
@@ -1,12 +1,12 @@
 # NFS server for democratic-csi
-# Exports ZFS datasets to k3s worker nodes over the LAN
+# Exports ZFS datasets to k3s worker nodes over the LAN and microvm bridge
 { config, lib, pkgs, ... }: {
 
   services.nfs.server = {
     enable = true;
     exports = ''
-      /fastPool/k3s/nfs  192.168.2.0/24(rw,no_subtree_check,no_root_squash,async)
-      /slowPool/k3s/nfs  192.168.2.0/24(rw,no_subtree_check,no_root_squash,async)
+      /fastPool/k3s/nfs  192.168.2.0/24(rw,no_subtree_check,no_root_squash,async) 10.100.0.0/24(rw,no_subtree_check,no_root_squash,async)
+      /slowPool/k3s/nfs  192.168.2.0/24(rw,no_subtree_check,no_root_squash,async) 10.100.0.0/24(rw,no_subtree_check,no_root_squash,async)
     '';
   };
 
